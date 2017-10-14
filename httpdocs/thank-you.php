644fa1533a889b7dbdd0e6e7c824ac1bcb9d4818
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2017-10-14 15:24:58 -0700

    replaced procedural code with the class that we have tested

--- httpdocs/thank-you.php
+++ httpdocs/thank-you.php
@@ -1,30 +1,18 @@
 <?php
 session_start();
 
-// check for required fields
-$_SESSION['error'] = [];
-if (!isset($_POST['fname']) || $_POST['fname'] == "") {
-    $_SESSION['error']['fname'] = "First Name is Required";
-}
-
-if (!isset($_POST['lname']) || $_POST['lname'] == "") {
-    $_SESSION['error']['lname'] = "Last Name is Required";
-}
-
-if (!isset($_POST['phone']) || $_POST['phone'] == "") {
-    $_SESSION['error']['phone'] = "Phone is Required";
-}
-
-if (!isset($_POST['email']) || $_POST['email'] == "") {
-    $_SESSION['error']['email'] = "Email Address is Required";
-}
-
-if (!isset($_POST['message']) || $_POST['message'] == "") {
-    $_SESSION['error']['message'] = "Message is Required";
-}
-
-// redirect if there are any errors
-if (count($_SESSION['error'])) {
+require "../Validator.php";
+$v = new \phpdx\Validator;
+$rules = [
+    'fname' => 'required',
+    'lname' => 'required',
+    'phone' => 'required',
+    'email' => 'required',
+    'message' => 'required',
+];
+$v->setRules($rules);
+if (!$v->validate($_POST)) {
+    $_SESSION['error'] = $v->errorMessages;
     header("Location: /contact.php");
     die();
 }
